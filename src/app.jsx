9335d499b74f76b855a96deeb69b481eb4ada830
emil.buraczynski <emil.buraczynski@decsoft.com.pl>
2019-03-08 15:49:19 +0100

    update jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import Countdown from './countdown'
+import React , { Component } from 'react';
+import './App.css';
+import './Countdown.css';
+import './EditEvent.css';
+import Countdown from './countdown';
+import EditEvent from './EditEvent';
 
-const app = () =>  (
-    <div>
-        <Countdown name="śniadanie" time="7:00"/>
-        <Countdown name="obiad" time="15:00"/>
-    </div>
-)
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            events:[
+               {id:1, name:'śniadanie', time:"7:00"},
+               {id:2, name:'obiad', time:"15:00"},
+               {id:3, name:'obiad', time:"15:00"}
+            ] 
+        }
+    }
+    render() {
+        const events = this.state.events.map( el => {
+            return  <Countdown key={el.id} name={el.name} time={el.time}/>
+        })
+        return (
+            <div className="app">
+             {events}
+             <EditEvent/>
+              </div>
+        )
+    }
+}
 
-export default app
+
+export default App
--- src/countdown.jsx
+++ src/countdown.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Countdown = props => (
     <div>
-        <div classname="countdown">
-            <strong>{props.name} </strong> { props.time}
+        <div className="countdown">
+      <strong>{props.name} </strong> { props.time}
         </div>
     </div>
 )
+
+Countdown.propTypes = {
+    name:PropTypes.string,
+    time:PropTypes.string
+}
+
 
 export default Countdown;
