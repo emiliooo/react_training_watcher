f18b552b218b29b71c9ddd3c550ce9431a8383b8
Emil <emilburaczynski@gmail.com>
2019-03-20 21:28:53 +0100

    updated

--- src/EditEvent.jsx
+++ src/EditEvent.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 
-const EditEvent = () => {
-    return(
+const EditEvent = props => {
+    return (
         <div className="edit-event">
             <div className="edit-event__input-group">
+                <p>
                     <label htmlFor="name">name</label>
-                    <input type="text" id="name" name="name"/>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    onChange={ e => props.onInputChange({[e.target.name]:e.target.value})}
+                    name="name" />
+                </p>
+
             </div>
             <div className="edit-event__input-group">
+                <p>
                     <label htmlFor="hour">hour</label>
-                    <input type="tel" id="hour" name="hour"/>
+                    <input 
+                    type="tel" 
+                    id="hour" 
+                    onChange={ e => props.onInputChange(e.target.name,e.target.value)}
+                    name="hour" />
+                </p>
             </div>
             <div className="edit-event__input-group">
+                <p>
                     <label htmlFor="minute">minute</label>
-                    <input type="tel" id="minute" name="minute"/>
+                    <input 
+                    type="tel" 
+                    id="minute" 
+                    onChange={ e =>  props.onInputChange(e.target.name,e.target.value)}
+                    name="minute" />
+                </p>
+
             </div>
             <button>ok</button>
             <button>cancel</button>
--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './Countdown.css';
 import './EditEvent.css';
@@ -9,25 +9,30 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            events:[
-               {id:1, name:'śniadanie', time:"7:00"},
-               {id:2, name:'obiad', time:"15:00"},
-               {id:3, name:'obiad', time:"15:00"}
-            ] 
+            events: [
+                { id: 1, name: 'śniadanie', time: "7:00" },
+                { id: 2, name: 'obiad', time: "15:00" },
+                { id: 3, name: 'obiad', time: "15:00" }
+            ]
         }
+        this.handleEditEvent = this.handleEditEvent.bind(this);
+    };
+
+    handleEditEvent(val) {
+        console.log(val);
     }
     render() {
-        const events = this.state.events.map( el => {
-            return  <Countdown key={el.id} name={el.name} time={el.time}/>
+        const events = this.state.events.map(el => {
+            return <Countdown key={el.id} name={el.name} time={el.time} />
         })
         return (
             <div className="app">
-             {events}
-             <EditEvent/>
-              </div>
+                {events}
+                <EditEvent 
+                onInputChange={val => this.handleEditEvent(val)}/>
+            </div>
         )
     }
 }
-
 
 export default App
